ace9c1f851f3624e0419f74b0560441c8e88f1d1
Brandon Smith <crescendobs@yahoo.com>
2023-02-07 05:08:28 -0600

    adding Palette.js

---
+++ src/JS/Palette.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+
+class Palette extends PureComponent {
+
+    render() {
+        return (
+            <div className='Palette'>
+                <h1></h1>
+            </div>
+        )
+    }
+}
+
+export default Palette
