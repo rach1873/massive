0aa72ff905dfafbb7db603753143c861cc08979d
Brandon Smith <crescendobs@yahoo.com>
2023-02-07 21:08:14 -0600

    adding overlay part 2

--- src/JS/ColorBox.js
+++ src/JS/ColorBox.js
@@ -19,7 +19,11 @@ class ColorBox extends Component {
         return (
             <CopyToClipboard text={this.props.background} onCopy={this.changeCopyState}>
                 <div style={{ background: this.props.background }} className='ColorBox'>
-                    <div style={{ background: this.props.background }} className='copy-overlay'></div>
+                    <div style={{ background: this.props.background }} className={`copy-overlay ${this.state.copied && "show"}`}></div>
+                    <div className={`copy-msg ${this.state.copied && "show"}`}>
+                        <h1>Copied!</h1>
+                        <p>{this.props.background}</p>
+                    </div>
                     <span>{this.props.name}</span>
                     <p className='Copy'>COPY</p>
                     <p className='More'>MORE</p>
