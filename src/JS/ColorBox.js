d9f6a3b44ec4f44904eb07fa64121fac565bcfe7
Brandon Smith <crescendobs@yahoo.com>
2023-02-07 20:38:22 -0600

    adding animation part 1

--- src/JS/ColorBox.js
+++ src/JS/ColorBox.js
@@ -3,11 +3,23 @@ import '../CSS/ColorBox.css'
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 class ColorBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { copied: false }
+        this.changeCopyState = this.changeCopyState.bind(this);
+    }
+
+    changeCopyState() {
+        this.setState({ copied: true }, () => {
+            setTimeout(() => this.setState({ copied: false }), 1500)
+        })
+    }
 
     render() {
         return (
-            <CopyToClipboard text={this.props.background}>
+            <CopyToClipboard text={this.props.background} onCopy={this.changeCopyState}>
                 <div style={{ background: this.props.background }} className='ColorBox'>
+                    <div style={{ background: this.props.background }} className='copy-overlay'></div>
                     <span>{this.props.name}</span>
                     <p className='Copy'>COPY</p>
                     <p className='More'>MORE</p>
